6ef84a1aa9dadd0364b46d0d87aedffd3141ba64
davc25 <150166551+davc25@users.noreply.github.com>
2025-08-01 19:03:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -46,11 +46,9 @@ function respondYes() {
     document.getElementById('message').innerText = "Yay! Gi accept na niya nga gwapa siyaaa!!! ❤️🥰";
 
     // Change background to GIF
-    document.body.style.background = "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2FxaDFvZmNsZWszYmJ3anczZXUxejdvNnlsZXJsM3hrNTYzYm1rayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H5PUsFQn7YEAlXgL5w/giphy.gif')";
-    document.body.style.backgroundSize = "70% auto";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundPosition = "center center";
-
+    <div class="gif-container">
+  <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2FxaDFvZmNsZWszYmJ3anczZXUxejdvNnlsZXJsM3hrNTYzYm1rayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H5PUsFQn7YEAlXgL5w/giphy.gif" class="background-gif">
+    </div>
     // Hide Yes and No buttons
     document.getElementById('yes-button').style.display = "none";
     document.getElementById('no-button').style.display = "none";
